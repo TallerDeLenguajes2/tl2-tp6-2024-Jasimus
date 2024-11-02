91eae2d097740cbf0c7ff109684fde6c75283f34
asd <asd>
2024-11-02 12:05:23 -0300

    agregación de vistas

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -5,7 +5,33 @@ using ProductoRepository_space;
 [ApiController]
 [Route("Productos")]
 
-class ProductosController : ControllerBase
+public class ProductosController : ControllerBase
 {
-    
+    ProductoRepository pr = new ProductoRepository();
+    [HttpGet]
+    public IActionResult ListarProductos()
+    {
+        return View(pr.ListarProductos);
+    }
+
+    [HttpPost]
+    public IActionResult CrearProducto(Producto producto)
+    {
+        int cant = pr.CrearProducto(producto);
+        return View(cant);
+    }
+
+    [HttpPut]
+    public IActionResult ModificarProducto(int id, Producto producto)
+    {
+        int cant = pr.ModificarProducto(id, producto);
+        return View(cant);
+    }
+
+    [HttpDelete]
+    public IActionResult EliminarProducto(int id)
+    {
+        int cant = pr.EliminarProducto(id);
+        return View(cant);
+    }
 }
